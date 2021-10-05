b2f46a6f98744891e8b705f9f52d10406b96ec25
HPSSupport <44605379+HPSSupport@users.noreply.github.com>
2021-10-05 09:35:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var xhttp = new XMLHttpRequest();
 xhttp.open("GET", "log.xml", false);
 xhttp.send();
 xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    
     myFunction(this);
-    }
+    
 };
 
 function myFunction(xml) {
