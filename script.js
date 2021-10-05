95ed223310340375d3c7043f510f25727a4be56d
HPSSupport <44605379+HPSSupport@users.noreply.github.com>
2021-10-05 09:49:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function add(){
-	$('#chargers tbody').append('<div><td>'+document.getElementById('date').value+'</td><td>'+document.getElementById('grade').value+'</td><td>'+document.getElementById('name').value+'</td><input type="button" value="Remove" onclick="remove()"></div>');
+	$('#chargers tbody').append('<div>'+'<td>'+document.getElementById('date').value+'</td>'+'<td>'+document.getElementById('grade').value+'</td>'+'<td>'+document.getElementById('name').value+'</td>'+'<input type="button" value="Remove" onclick="remove()">'+'</div>');
 }
