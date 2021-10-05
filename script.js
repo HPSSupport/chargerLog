ea295253f39bb9348e4425a269acfaad48eac992
HPSSupport <44605379+HPSSupport@users.noreply.github.com>
2021-10-05 09:24:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ xhttp.onreadystatechange = function() {
     myFunction(this);
     }
 };
-xhttp.open("GET", "books.xml", true);
+xhttp.open("GET", "log.xml", true);
 xhttp.send();
 
 function myFunction(xml) {
     var xmlDoc = xml.responseXML;
-    document.getElementById("demo").innerHTML =
-    xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
 }
 
 for(i = 0; i <= xmlDoc.getElementsByTagName('root').length; i++){
