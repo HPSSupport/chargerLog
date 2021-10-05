05b642be449b3ada031f59b246a05cdb972798e3
HPSSupport <44605379+HPSSupport@users.noreply.github.com>
2021-10-05 09:21:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
-	console.log(xhttp.responseXML);
+	console.log(xhttp.readyState + ' '+ xhttp.status);
 if (xhttp.readyState == 4 && xhttp.status == 200) {
 
     var xmlDoc = xml.responseXML;
