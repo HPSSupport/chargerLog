c2364365f6a0b86724bea46ed690cc4c7c4c1991
HPSSupport <44605379+HPSSupport@users.noreply.github.com>
2021-10-05 11:02:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+window.addEventListener('beforeunload', function (e) {
+  // Cancel the event
+  e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+  // Chrome requires returnValue to be set
+  e.returnValue = 'oh no';
+});
 function add(){
-	$('#chargers tbody').append('<div>'+'<td>'+document.getElementById('date').value +'</td>'+'<td>'+document.getElementById('grade').value +'</td>'+'<td>'+document.getElementById('name').value +'</td>'+'<input type="button" value="Remove" onclick="remove()">'+'</div>');
+	alert("Page refresh will remove all listings!");
+	var toSet = '<tr>'+'<td>' + document.getElementById("date").value + '</td>' + '<td>'+document.getElementById("grade").value + '</td>' + '<td class="final">' + document.getElementById("name").value + '</td>'+'</tr>';
+	$('#chargers tbody').append(toSet);
+	$('#chargers tbody tr').append('<td><input type="button" value="Remove" onclick="remove(this)"></td>');
+	document.getElementById("date").value = '';
+	document.getElementById("grade").value = '';
+	document.getElementById("name").value = '';
+}
+function remove(e){
+	e.parentElement.parentElement.remove();
 }
