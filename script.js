fccaf908bc532c299a0593b65d805b239e8d7720
HPSSupport <44605379+HPSSupport@users.noreply.github.com>
2021-10-05 09:27:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ xhttp.onreadystatechange = function() {
     myFunction(this);
     }
 };
-xhttp.open("GET", "log.xml", true);
+xhttp.open("GET", "log.xml", false);
 xhttp.send();
 
 function myFunction(xml) {
